836074d9e75ac91521a2b6dc7c12576b3b72e3e0
akashr220007 <akashr220007@gmail.com>
2026-02-03 16:57:42 +0530

    i

--- main.tf
+++ main.tf
@@ -61,11 +61,52 @@ resource "aws_instance" "web" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t2.micro"
   subnet_id              = "subnet-0d69a7a76e6b17f90"
-  vpc_security_group_ids = ["sg-02b85c8ea3900543a"]
+  vpc_security_group_ids ="sg-07b8e6b02da3caa3c"
+  associate_public_ip_address = true
   key_name               = "awskey"
   user_data              = file("${path.module}/user_data.sh")
   
   tags = {
     Name = "web-server"
   }
-}         
+}
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "akash-my-terraform-s3-bucket-12345"
+  
+  tags = {
+    Name        = "MyTerraformBucket"
+    Environment = "Dev"
+  }
+}
+
+esource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
